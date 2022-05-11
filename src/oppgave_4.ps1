5823910c4b5ae60c33e8abb6aac7b9cb70e4bb2f
g102798 <ivar.erik.gjertsen@nav.no>
2022-05-11 10:42:35 +0200

    Rettet igjen

--- src/oppgave_4.ps1
+++ src/oppgave_4.ps1
@@ -6,10 +6,8 @@ param (
     # når paramater ikke er gitt brukes default verdi
     $UrlKortstokk = 'http://nav-deckofcards.herokuapp.com/shuffle'
 )
-
-$webRequest = Invoke-WebRequest -Uri $UrlKortstokk
 $ErrorActionPreference = 'Stop'
-
+$webRequest = Invoke-WebRequest -Uri $UrlKortstokk
 
 
 $kortstokkJson = $webRequest.Content
@@ -20,16 +18,16 @@ $kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
 
 
 
-foreach ($kort in $kortstokk) {
+<# foreach ($kort in $kortstokk) {
     Write-Output $kort
 }
-
+ #>
 # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_operators?view=powershell-7.1#subexpression-operator--
 
-foreach ($kort in $kortstokk) {
-    Write-Output "$($kort.suit[0])+$($kort.value)"
+<# foreach ($kort in $kortstokk) {
+    Write-Output "$($kort.suit[0])$($kort.value)"
 }
-
+ #>
 # 3. utgave - ønsker egentlig hele kortstokken som en streng og den koden som en funksjon (gjenbruk)
 
 function kortstokkTilStreng {
@@ -48,63 +46,3 @@ function kortstokkTilStreng {
 
 Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
 
-# hvorfor kommer det et komma ',' etter siste kort?
-# frivillig oppgave - kan du forbedre funksjonen 'kortTilStreng' - ikke skrive ut komma etter siste kort?
-
-### Regn ut den samlede poengsummen til kortstokk
-#   Nummererte kort har poeng som angitt på kortet
-#   Knekt (J), Dronning (Q) og Konge (K) teller som 10 poeng
-#   Ess (A) teller som 11 poeng
-
-# 1. - utgave - summen av poeng for kort er form for loop/iterere oppgave
-
-$poengKortstokk = # god startverdi?
-
-# hva er forskjellen mellom -eq, ieg og ceq?
-# # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-7.2
-
-foreach ($kort in $kortstokk) {
-    if ($kort.value -ceq 'J') {
-        $poengKortstokk = $poengKortstokk + 10
-    }
-    elseif ($kort.value -ceq '?') {
-        $poengKortstokk = $poengKortstokk + 10
-    }
-    elseif ($kort.value -ceq '?') {
-        $poengKortstokk = $poengKortstokk + 10
-    }
-    elseif ($kort.value -ceq '?') {
-        $poengKortstokk = $poengKortstokk + 11
-    }
-    else {
-        $poengKortstokk = #?
-    }
-}
-
-Write-Host <# ? #>
-
-# 2. utgave - ønsker koden som en funksjon - hvorfor?
-
-# https://docs.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-switch?view=powershell-7.1
-
-function sumPoengKortstokk {
-    [OutputType([int])]
-    param (
-        [object[]]
-        $kortstokk
-    )
-
-    $poengKortstokk = 0
-
-    foreach ($kort in $kortstokk) {
-        # Undersøk hva en Switch er
-        $poengKortstokk += switch ($kort.value) {
-            { $_ -cin @('J', <#?#>) } { 10 }
-            'A' { <#?#> }
-            default { $kort.value }
-        }
-    }
-    return $poengKortstokk
-}
-
-Write-Output "Poengsum: $(sumPoengKortstokk -kortstokk $kortstokk)"
